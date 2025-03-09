84154b6d549f2adcfd1d7a7baaf48b69e4469f6b
Mohammed Abrar Shariar <mshariar@k1r1p11.42mulhouse.fr>
2025-03-09 22:09:38 +0100

    removed

--- srcs/key_hooks.c
+++ srcs/key_hooks.c
@@ -3,70 +3,53 @@
 /*                                                        :::      ::::::::   */
 /*   key_hooks.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: my42 <my42@student.42.fr>                  +#+  +:+       +#+        */
+/*   By: mshariar <mshariar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/06 22:00:33 by mshariar          #+#    #+#             */
-/*   Updated: 2025/03/09 04:44:38 by my42             ###   ########.fr       */
+/*   Updated: 2025/03/08 21:40:52 by mshariar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+
 #include "so_long.h"
-#include <time.h>  // For srand/rand
 
 int	quit_game(t_game *game)
 {
-    destroy(game);
-    exit(EXIT_SUCCESS);
-    return (EXIT_SUCCESS);
+	destroy(game);
+	exit(EXIT_SUCCESS);
+	return (EXIT_SUCCESS);
 }
 
 int	on_press(int key, t_game *game)
 {
-    if (key == ESC)
-        quit_game(game);
-    else if (key == W || key == UP)
-        update_player_pos(game, false, -1);
-    else if (key == A || key == LEFT)
-        update_player_pos(game, true, -1);
-    else if (key == S || key == DOWN)
-        update_player_pos(game, false, 1);
-    else if (key == D || key == RIGHT)
-        update_player_pos(game, true, 1);
-    return (EXIT_SUCCESS);
-}
-
-int	enemy_loop(t_game *game)
-{
-    // Move enemies on their own every frame
-    if (game->map.enemy > 0)
-        move_enemy(game);
-    return (EXIT_SUCCESS);
+	if (key == ESC)
+		quit_game(game);
+	else if (key == W || key == UP)
+		update_player_pos(game, false, -1);
+	else if (key == A || key == LEFT)
+		update_player_pos(game, true, -1);
+	else if (key == S || key == DOWN)
+		update_player_pos(game, false, 1);
+	else if (key == D || key == RIGHT)
+		update_player_pos(game, true, 1);
+	return (EXIT_SUCCESS);
 }
 
 void	hook_n_run(t_game *game)
 {
-    // Initialize random seed for enemy movement
-    srand(time(NULL));
-    
-    mlx_hook(game->win_ptr, KEYPRESS_EVENT, (1L << 0), on_press, game);
-    mlx_hook(game->win_ptr, DESTROY_NOTIFY_EVENT, (1L << 17),
-        quit_game, game);
-        
-    // Add loop hook for continuous enemy movement (optional)
-    // Uncomment to have enemies move continuously
-    // mlx_loop_hook(game->mlx_ptr, enemy_loop, game);
-    
-    mlx_loop(game->mlx_ptr);
+	mlx_hook(game->win_ptr, KEYPRESS_EVENT, (1L << 0), on_press, game);
+	mlx_hook(game->win_ptr, DESTROY_NOTIFY_EVENT, (1L << 17),
+		quit_game, game);
+	mlx_loop(game->mlx_ptr);
 }
-
 void	init_mlx(t_game *game)
 {
-    game->mlx_ptr = mlx_init();
-    if (!game->mlx_ptr)
-        write_error(game, MLX_INIT_ERR);
-    game->win_ptr = mlx_new_window(game->mlx_ptr,
-            game->map.columns * TILE_SIZE, game->map.rows * TILE_SIZE,
-            "so_long");
-    if (!game->win_ptr)
-        write_error(game, MLX_NEW_WINDOW_ERR);
+	game->mlx_ptr = mlx_init();
+	if (!game->mlx_ptr)
+		write_error(game, MLX_INIT_ERR);
+	game->win_ptr = mlx_new_window(game->mlx_ptr,
+			game->map.columns * TILE_SIZE, game->map.rows * TILE_SIZE,
+			"so_long");
+	if (!game->win_ptr)
+		write_error(game, MLX_NEW_WINDOW_ERR);
 }
--- srcs/map/valid_map.c
+++ srcs/map/valid_map.c
@@ -3,120 +3,92 @@
 /*                                                        :::      ::::::::   */
 /*   valid_map.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: my42 <my42@student.42.fr>                  +#+  +:+       +#+        */
+/*   By: mshariar <mshariar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/06 22:03:37 by mshariar          #+#    #+#             */
-/*   Updated: 2025/03/09 04:39:16 by my42             ###   ########.fr       */
+/*   Updated: 2025/03/08 21:40:35 by mshariar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+
 #include "../../includes/so_long.h"
 
-#define VALID_ENTITIES "ECP01B"
+#define VALID_ENTITIES "ECP01"
 
 static void	throw_error_if(t_game *game)
 {
-    if (game->map.exit == 0 || game->map.exit > 1)
-        write_error(game, INVALID_NBR_EXITS);
-    if (game->map.collectibles == 0)
-        write_error(game, NO_COLLECTIBLES);
-    if (game->map.player == 0 || game->map.player > 1)
-        write_error(game, INVALID_NBR_PLAYERS);
-}
-
-static void	count_enemies(t_game *game)
-{
-    int	i;
-    int	j;
-    int	enemy_idx;
-
-    if (game->map.enemy > 0)
-    {
-        game->map.enemy_pos = malloc(sizeof(t_point) * game->map.enemy);
-        if (!game->map.enemy_pos)
-            write_error(game, MALLOC_ERR);
-        enemy_idx = 0;
-        i = -1;
-        while (++i < game->map.rows)
-        {
-            j = -1;
-            while (++j < game->map.columns)
-                if (game->map.map[i][j] == ENEMY)
-                {
-                    game->map.enemy_pos[enemy_idx] = (t_point){j, i};
-                    enemy_idx++;
-                }
-        }
-    }
+	if (game->map.exit == 0 || game->map.exit > 1)
+		write_error(game, INVALID_NBR_EXITS);
+	if (game->map.collectibles == 0)
+		write_error(game, NO_COLLECTIBLES);
+	if (game->map.player == 0 || game->map.player > 1)
+		write_error(game, INVALID_NBR_PLAYERS);
 }
 
 static void	check_elements(t_game *game)
 {
-    int	i;
-    int	j;
+	int	i;
+	int	j;
 
-    i = -1;
-    while (++i < game->map.rows)
-    {
-        j = -1;
-        while (++j < game->map.columns)
-        {
-            if (!is_onstr(VALID_ENTITIES, game->map.map[i][j]))
-                write_error(game, INVALID_ENTITY);
-            if (game->map.map[i][j] == EXIT)
-                game->map.exit += 1;
-            else if (game->map.map[i][j] == COLLECTIBLE)
-                game->map.collectibles += 1;
-            else if (game->map.map[i][j] == PLAYER)
-            {
-                game->map.player += 1;
-                game->map.player_pos = (t_point){j, i};
-            }
-            else if (game->map.map[i][j] == ENEMY)
-                game->map.enemy += 1;
-        }
-    }
-    count_enemies(game);
-    throw_error_if(game);
+	i = -1;
+	while (++i < game->map.rows)
+	{
+		j = -1;
+		while (++j < game->map.columns)
+		{
+			if (!is_onstr(VALID_ENTITIES, game->map.map[i][j]))
+				write_error(game, INVALID_ENTITY);
+			if (game->map.map[i][j] == EXIT)
+				game->map.exit += 1;
+			else if (game->map.map[i][j] == COLLECTIBLE)
+				game->map.collectibles += 1;
+			else if (game->map.map[i][j] == PLAYER)
+			{
+				game->map.player += 1;
+				game->map.player_pos = (t_point){j, i};
+			}
+		}
+	}
+	throw_error_if(game);
 }
 
 static bool	is_closed(t_map *map)
 {
-    int	i;
+	int	i;
 
-    i = -1;
-    while (++i < map->rows)
-        if (map->map[i][0] != WALL || map->map[i][map->columns - 1] != WALL)
-            return (false);
-    i = -1;
-    while (++i < map->columns)
-        if (map->map[0][i] != WALL || map->map[map->rows - 1][i] != WALL)
-            return (false);
-    return (true);
+	i = -1;
+	while (++i < map->rows)
+		if (map->map[i][0] != WALL || map->map[i][map->columns - 1] != WALL)
+			return (false);
+	i = -1;
+	while (++i < map->columns)
+		if (map->map[0][i] != WALL || map->map[map->rows - 1][i] != WALL)
+			return (false);
+	return (true);
 }
 
 static bool	valid_form(t_game *game)
 {
-    size_t	len;
-    size_t	i;
+	size_t	len;
+	size_t	i;
 
-    len = game->map.columns;
-    i = 0;
-    while (game->map.map[i] != NULL)
-    {
-        if (len != ft_strlen(game->map.map[i]))
-            return (false);
-        i += 1;
-    }
-    return (true);
+	len = game->map.columns;
+	i = 0;
+	while (game->map.map[i] != NULL)
+	{
+		if (len != ft_strlen(game->map.map[i]))
+			return (false);
+		i += 1;
+	}
+	return (true);
 }
 
 void	map_check(t_game *game)
 {
-    if (!valid_form(game))
-        write_error(game, INVALID_FORMAT);
-    check_elements(game);
-    if (!is_closed(&game->map))
-        write_error(game, MAP_NOT_CLOSED);
-    validate_path(game);
+	if (!valid_form(game))
+		write_error(game, INVALID_FORMAT);
+	check_elements(game);
+	if (!is_closed(&game->map))
+		write_error(game, MAP_NOT_CLOSED);
+	validate_path(game);
 }
